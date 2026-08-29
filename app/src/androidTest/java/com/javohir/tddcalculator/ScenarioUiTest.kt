036d56fe1064927c28a0440e20a00fd01875e586
Javohir <oromovjavoxir@gmail.com>
2026-08-29 12:19:19 +0500

    Initial commit

---
+++ app/src/androidTest/java/com/javohir/tddcalculator/ScenarioUiTest.kt
@@ -0,0 +1,41 @@
+package com.javohir.tddcalculator
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Created by: Javohir Oromov macos
+ * Project: TDDCalculator
+ * Package: com.javohir.tddcalculator
+ * Description: 
+ */
+@RunWith(value = AndroidJUnit4::class)
+class ScenarioUiTest {
+
+    @get: Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    private val mainPage = MainPage(composeTestRule)
+
+    @Test
+    fun sum_of_two_numbers(){
+        mainPage.clickNumberOneButton()
+        mainPage.assertInputField(expected = "1")
+
+        mainPage.clickOperationPlusButton()
+        mainPage.assertInputField(expected = "1+")
+
+        mainPage.clickNumberTwoButton()
+        mainPage.assertInputField(expected = "1+2")
+
+        mainPage.clickEqualsButton()
+        mainPage.assertInputField(expected = "1+2")
+        mainPage.assertResult(expected = "3")
+    }
+
+    
+}
+
